c76167a20bad086e7962fe9ef6d88b0476a7c8c7
lars <koninglars04@gmail.com>
2023-06-14 14:11:50 +0200

    touch ups

--- ipmedt4/app/Http/Controllers/ReserveringenController.php
+++ ipmedt4/app/Http/Controllers/ReserveringenController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Models\Reservering;
+use App\Models\Reservering;
 
 class ReserveringenController extends Controller
 {
--- ipmedt4/app/Models/Reservering.php
+++ ipmedt4/app/Models/Reservering.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservering extends Model
 {
-    use HasFactory;
+    protected $table = "reserveringen";
 }
