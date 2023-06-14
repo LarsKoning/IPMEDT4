95810b63cc9e34818496d6380e13cc42472db78f
lars <koninglars04@gmail.com>
2023-06-14 10:27:47 +0200

    back-end af

--- ipmedt4/app/Http/Controllers/ReserveringenController.php
+++ ipmedt4/app/Http/Controllers/ReserveringenController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Models\Reservering;
 
 class ReserveringenController extends Controller
 {
-    //
+    public function index() {
+        return Reservering::all();
+    }
 }
--- ipmedt4/app/Http/Controllers/UsersController.php
+++ ipmedt4/app/Http/Controllers/UsersController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Models\User;
+
 
 class UsersController extends Controller
 {
-    //
+    public function index() {
+        return User::all();
+    }
 }
